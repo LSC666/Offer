b3a033da88bacf83059b84db85dc030acc69e720
lsc <lsc000@bupt.edu.cn>
2020-10-26 16:20:13 +0800

    offer06添加递归方法

--- code/offer06.go
+++ code/offer06.go
@@ -1,30 +1,29 @@
 package code
 
-
 //Definition for singly-linked list.
 type ListNode struct {
-    Val int
-    Next *ListNode
+	Val  int
+	Next *ListNode
 }
 
 /*
 创建无长度的slice，遍历链表，将节点值存入slice中，最后反转slice即可。
- */
+*/
 func reversePrint(head *ListNode) []int {
-	if head==nil{
+	if head == nil {
 		return nil
 	}
 	var res []int
-	p:=head
-	for p!=nil {
-		res=append(res, p.Val)
-		p=p.Next
+	p := head
+	for p != nil {
+		res = append(res, p.Val)
+		p = p.Next
 	}
-	l:=len(res)
-	for i:=0;i<l/2;i++ {
-		t:=res[i]
-		res[i]=res[l-1-i]
-		res[l-1-i]=t
+	l := len(res)
+	for i := 0; i < l/2; i++ {
+		t := res[i]
+		res[i] = res[l-1-i]
+		res[l-1-i] = t
 	}
 	return res
 }
@@ -32,24 +31,35 @@ func reversePrint(head *ListNode) []int {
 /*
 先遍历链表，统计链表长度，根据长度创建slice，
 遍历链表，将节点值依次存入slice的末端。
- */
+*/
 func func06(head *ListNode) []int {
-	if head==nil{
+	if head == nil {
 		return nil
 	}
-	l:=0
-	p:=head
-	for p!=nil {
+	l := 0
+	p := head
+	for p != nil {
 		l++
-		p=p.Next
+		p = p.Next
 	}
-	res:=make([]int,l)
-	p=head
-	i:=l-1
-	for p!=nil {
-		res[i]=p.Val
-		p=p.Next
+	res := make([]int, l)
+	p = head
+	i := l - 1
+	for p != nil {
+		res[i] = p.Val
+		p = p.Next
 		i--
 	}
 	return res
+}
+
+/*
+递归
+*/
+func func06_2(head *ListNode) []int {
+	if head == nil {
+		return []int{}
+	}
+	res := func06_2(head.Next)
+	return append(res, head.Val)
 }
